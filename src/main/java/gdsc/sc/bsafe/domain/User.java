08dd045339f120d53131c939af2d37775be11122
jiwon <kiwijomn@gmail.com>
2024-01-18 03:20:22 +0900

    :recycle: separate UserDetails into CustomUserDetails class

--- src/main/java/gdsc/sc/bsafe/domain/User.java
+++ src/main/java/gdsc/sc/bsafe/domain/User.java
@@ -1,13 +1,9 @@
 package gdsc.sc.bsafe.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static gdsc.sc.bsafe.domain.Authority.ROLE_USER;
 
@@ -15,7 +11,8 @@ import static gdsc.sc.bsafe.domain.Authority.ROLE_USER;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user")
-public class User implements UserDetails {
+public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -33,7 +30,6 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String name;
 
-    @Builder.Default
     @Enumerated(EnumType.STRING)
     private Authority authority = ROLE_USER;
 
@@ -44,28 +40,4 @@ public class User implements UserDetails {
         this.name = name;
         this.authority = ROLE_USER;
     }
-
-    // UserDetails
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-        authorities.add(new SimpleGrantedAuthority(this.authority.toString()));
-        return authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-    @Override
-    public String getUsername() {  return String.valueOf(this.userId); }
-    @Override
-    public boolean isAccountNonExpired() { return true; }
-    @Override
-    public boolean isAccountNonLocked() { return true; }
-    @Override
-    public boolean isCredentialsNonExpired() { return true; }
-    @Override
-    public boolean isEnabled() {return true; }
 }
-
--- src/main/java/gdsc/sc/bsafe/repository/UserRepository.java
+++ src/main/java/gdsc/sc/bsafe/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByNickname(String nickname);
     Optional<User> findByEmail(String email);
 }
