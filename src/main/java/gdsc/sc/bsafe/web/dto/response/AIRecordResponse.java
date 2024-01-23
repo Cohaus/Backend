98f0638607ecb8d163b9fc7edf1c368ecb7f81ec
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-01-24 01:06:38 +0900

    :pencil2: fix variable name

--- src/main/java/gdsc/sc/bsafe/web/dto/response/AIRecordResponse.java
+++ src/main/java/gdsc/sc/bsafe/web/dto/response/AIRecordResponse.java
@@ -1,23 +1,20 @@
 package gdsc.sc.bsafe.web.dto.response;
 
 import gdsc.sc.bsafe.domain.enums.RecordType;
-import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class AIRecordResponse {
-    private Long recordId;
+    private Long record_id;
 
     @Enumerated(value = EnumType.STRING)
     private RecordType type = RecordType.AI;
 
-    private Long userId;
+    private Long user_id;
 
     private String image;
 
@@ -28,8 +25,14 @@ public class AIRecordResponse {
     private String category;
 
     private Integer grade;
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/develop
+    public AIRecordResponse(Long recordId, Long userId, String image, String title, String detail, String category, Integer grade) {
+        this.record_id = recordId;
+        this.user_id = userId;
+        this.image = image;
+        this.title = title;
+        this.detail = detail;
+        this.category = category;
+        this.grade = grade;
+    }
 }
