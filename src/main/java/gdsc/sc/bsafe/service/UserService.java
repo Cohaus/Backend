4964957435f20725d58dfe565db238db42730a98
jiwon <kiwijomn@gmail.com>
2024-01-27 00:59:12 +0900

    :sparkles: add update user information feature

--- src/main/java/gdsc/sc/bsafe/service/UserService.java
+++ src/main/java/gdsc/sc/bsafe/service/UserService.java
@@ -1,21 +1,80 @@
 package gdsc.sc.bsafe.service;
 
 import gdsc.sc.bsafe.domain.User;
+import gdsc.sc.bsafe.domain.enums.Authority;
+import gdsc.sc.bsafe.domain.enums.VolunteerType;
+import gdsc.sc.bsafe.domain.mapping.Volunteer;
 import gdsc.sc.bsafe.global.exception.CustomException;
 import gdsc.sc.bsafe.global.exception.enums.ErrorCode;
 import gdsc.sc.bsafe.repository.UserRepository;
+import gdsc.sc.bsafe.repository.VolunteerRepository;
+import gdsc.sc.bsafe.web.dto.request.UpdateUserInfoRequest;
+import gdsc.sc.bsafe.web.dto.response.UpdateUserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
+    private final VolunteerRepository volunteerRepository;
+    private final VolunteerService volunteerService;
 
     public User findById(Long userId){
-        return userRepository.findById(userId).orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_USER));
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    }
+
+    @Transactional
+    public UpdateUserInfoResponse updateUserInfo(User user, UpdateUserInfoRequest request) {
+        User requestUser = findById(user.getUserId());
+        String userAuthority = request.getUser_authority();
+        String volunteerType = null;
+        String organizationName = null;
+
+        switch (Authority.valueOf(userAuthority)) {
+            case USER:
+                requestUser.updateUserInfo(request.getName(), request.getId(), request.getEmail(), request.getTel(), Authority.USER);
+                break;
+            case VOLUNTEER:
+                if (VolunteerType.SINGLE.toString().equals(request.getVolunteer_type())) {
+                    requestUser.updateUserInfo(request.getName(), request.getId(), request.getEmail(), request.getTel(), Authority.VOLUNTEER);
+                    Optional<Volunteer> volunteer = volunteerRepository.findByUser(requestUser);
+                    volunteer.ifPresentOrElse(
+                            volunteerUser -> volunteerUser.updateVolunteerType(VolunteerType.SINGLE),
+                            () -> volunteerRepository.save(Volunteer.builder().user(requestUser).type(VolunteerType.SINGLE).build())
+                    );
+                    volunteerType = VolunteerType.SINGLE.toString();
+                } else if (VolunteerType.ORGANIZATION.toString().equals(request.getVolunteer_type())) {
+                    requestUser.updateUserInfo(request.getName(), request.getId(), request.getEmail(), request.getTel(), Authority.VOLUNTEER);
+                    Optional<Volunteer> volunteer = volunteerRepository.findByUser(requestUser);
+                    volunteer.ifPresentOrElse(
+                            volunteerUser -> volunteerUser.updateVolunteerType(VolunteerType.ORGANIZATION),
+                            () -> volunteerRepository.save(Volunteer.builder().user(requestUser).type(VolunteerType.ORGANIZATION).build())
+                    );
+                    volunteer.ifPresent(vol -> volunteerService.updateVolunteerOrganization(vol, request.getOrganization_name()));
+                    volunteerType = VolunteerType.ORGANIZATION.toString();
+                    organizationName = request.getOrganization_name();
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid user authority: " + userAuthority);
+        }
+
+        return new UpdateUserInfoResponse(
+                requestUser.getUserId(),
+                requestUser.getName(),
+                requestUser.getId(),
+                requestUser.getEmail(),
+                requestUser.getTel(),
+                volunteerType,
+                organizationName
+        );
     }
 
     @Transactional
--- src/main/java/gdsc/sc/bsafe/web/controller/UserController.java
+++ src/main/java/gdsc/sc/bsafe/web/controller/UserController.java
@@ -4,23 +4,34 @@ import gdsc.sc.bsafe.domain.User;
 import gdsc.sc.bsafe.global.annotation.AuthenticationUser;
 import gdsc.sc.bsafe.global.common.SuccessResponse;
 import gdsc.sc.bsafe.service.UserService;
+import gdsc.sc.bsafe.web.dto.request.UpdateUserInfoRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "User API", description = "회원 정보 조회 / 회원 탈퇴")
+@Tag(name = "User API", description = "회원정보 수정 / 회원 탈퇴")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
+
+    @Operation(summary = "회원정보 수정 API", description = "요청 성공 시 회원 정보를 반환합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다.")
+    })
+    @PatchMapping("/info")
+
+    public ResponseEntity<SuccessResponse<?>> updateUserInfo(@AuthenticationUser User user,
+                                                             @Valid @RequestBody UpdateUserInfoRequest request) {
+        return SuccessResponse.ok(userService.updateUserInfo(user, request));
+    }
 
     @Operation(summary = "회원 탈퇴 API", description = "요청 성공 시 DB에서 회원 데이터를 삭제합니다.")
     @ApiResponses(value = {
