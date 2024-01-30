d999ddc27c46054e3c44bcb051a446f36bbe017e
jiwon <kiwijomn@gmail.com>
2024-01-30 21:50:04 +0900

    :memo: update swagger response for map api

--- src/main/java/gdsc/sc/bsafe/repository/projection/CountRepair.java
+++ src/main/java/gdsc/sc/bsafe/repository/projection/CountRepair.java
@@ -1,5 +1,6 @@
 package gdsc.sc.bsafe.repository.projection;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,13 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CountRepair {
 
+    @Schema(description = "법정동 pk", example = "1")
     private Long legalDistrict;
+
+    @Schema(description = "지역", example = "서울시 광진구 군자동")
     private String district;
+
+    @Schema(description = "해당 지역의 수리 요청 개수", example = "5")
     private long count;
 
     public CountRepair(Long legalDistrict, String sido, String gu, String dong, long count) {
