ef6f26ce6a1d59951bf96779abacc920a8ca8b77
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-02-06 00:01:16 +0900

    :necktie: add logic parsing gu of legal district to match the waste facility gu

--- src/main/java/gdsc/sc/bsafe/service/WasteFacilityService.java
+++ src/main/java/gdsc/sc/bsafe/service/WasteFacilityService.java
@@ -7,10 +7,12 @@ import gdsc.sc.bsafe.web.dto.request.DistrictRequest;
 import gdsc.sc.bsafe.web.dto.response.WasteFacilityListResponse;
 import gdsc.sc.bsafe.web.dto.response.WasteFacilityResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -20,8 +22,22 @@ public class WasteFacilityService {
 
     public WasteFacilityListResponse getNearWasteFacilityInfo(DistrictRequest request){
         String sido = request.getSido();
-        String gu = request.getGu();
+        String gu = getWasteFacilityGu(request.getGu());
         Slice<WasteFacility> wasteFacilities = wasteFacilityRepository.findWasteFacilitiesBySidoAndGu(sido,gu);
         return new WasteFacilityListResponse(new SliceResponse<>(wasteFacilities.map(WasteFacilityResponse::new)));
+    }
+
+    public String getWasteFacilityGu(String legalDistrictGu){
+        String wasteFacilityGu ;
+        // xx구 xx시
+        if(legalDistrictGu.contains(" ")){
+            String[] district = legalDistrictGu.split(" ");
+            wasteFacilityGu = district[0];
+        }
+        // xx시
+        else {
+            wasteFacilityGu =  legalDistrictGu;
+        }
+        return wasteFacilityGu;
     }
 }
