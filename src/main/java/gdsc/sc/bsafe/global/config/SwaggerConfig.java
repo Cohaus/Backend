c80194108992eb760b14f3309afb2b551fbfe9b7
jiwon <kiwijomn@gmail.com>
2024-02-06 03:48:45 +0900

    :memo: update swagger default server url

--- src/main/java/gdsc/sc/bsafe/global/config/SwaggerConfig.java
+++ src/main/java/gdsc/sc/bsafe/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -23,6 +24,10 @@ public class SwaggerConfig {
                 .version("v1.0")
                 .description("API 명세서입니다.");
 
+        Server server = new Server()
+                .url("https://cohaus.app")
+                .description("Cohaus Server url");
+
         SecurityRequirement securityRequirement = new SecurityRequirement()
                 .addList(KEY);
 
@@ -37,6 +42,7 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(info)
+                .addServersItem(server)
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
